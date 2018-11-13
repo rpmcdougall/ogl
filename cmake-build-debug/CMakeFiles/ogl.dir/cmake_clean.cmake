6bffd6971ef2e4ea99a0ca8b6a3d1e0d934e9fc1
Ryan McDougall <rp.mcdougall@gmail.com>
2018-11-12 18:36:30 -0600

    initial commit with dependencies added

---
+++ cmake-build-debug/CMakeFiles/ogl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ogl.dir/main.cpp.o"
+  "ogl.pdb"
+  "ogl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ogl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+using namespace glm;
+
+int main() {
+
+
+
+
+
+
+
+    return 0;
+}
